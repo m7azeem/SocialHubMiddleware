052f55418b8445a2b830c66c6a8f0623c98145b6
Balraj <polltery@gmail.com>
2017-03-12 19:46:26 +0400

    Update hasher methods

--- src/main/java/socialhubmiddleware/Hasher.java
+++ src/main/java/socialhubmiddleware/Hasher.java
@@ -1,13 +1,27 @@
 package socialhubmiddleware;
 
+import org.mindrot.jbcrypt.*;
+
 /*
  *  All hashing is to be done just before any DB CRUD operations. This is to keep consistency in the code.
 */
 public class Hasher {
 	
 	public static String hashPass(String password){
-		//do some hashing here
-		return password;
+
+		// Hash a password for the first time
+		String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+		return hashedPassword;
+	}
+
+	// Check that an unencrypted password matches one that has
+	// previously been hashed
+	public static boolean checkPass(String password, String hashedPassword){
+		if(BCrypt.checkpw(password, hashedPassword)){
+			return true;
+		}else{
+			return false;
+		}
 	}
 
 }
