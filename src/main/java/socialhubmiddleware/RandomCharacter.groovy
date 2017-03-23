7aafd915982184fceb5581f91335955744e71adb
apeqsha <ad55@hw.ac.uk>
2017-03-23 18:28:43 +0400

    Update and rename TokenGenerator.java to RandomCharacter.groovy

---
+++ src/main/java/socialhubmiddleware/RandomCharacter.groovy
@@ -0,0 +1,19 @@
+/* Author(s) : Apeqsha
+ * Date : 21/03/2017
+ */
+
+class RandomCharacter { 
+   static void main(String[] args) { 
+      String u = "Username";
+      String p = "Password";
+      String r = u+p;
+      Integer length = 20;
+      char[] random = r.toCharArray();
+      
+      char[] key
+          
+      for (def i=0;i<256;i++){
+       key[i]= new Random().with{random[i].join()};
+       }
+        println(key);   
+       }}
