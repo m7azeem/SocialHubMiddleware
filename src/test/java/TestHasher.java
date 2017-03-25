56f9e8b91fb1adbab60d9e932656ae2158d15ae7
apeqsha <ad55@hw.ac.uk>
2017-03-25 10:21:22 +0400

    Update TestHasher.java

--- src/test/java/TestHasher.java
+++ src/test/java/TestHasher.java
@@ -1,3 +1,40 @@
 /*
 JUnit tests for Hasher class
 */
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestForHasher {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test_classHasher1() {
+		//to find the Hash password for password = abcd;
+		String c="$2a$10$4rKRs4BEoCHTD.V1OqG5G.K0Ih5rjGZF8Lap1WSXRYiUIvkFSP4Qq";
+		Hasher.hashPass("abcd");
+		System.out.println(	Hasher.hashPass("abcd"));
+		//assertEquals(c, Hasher.hashPass("abcd"));
+	}
+    @Test
+	public void test_classHarsher2() {
+		//to check if the previous Hash password is equal to newly generated Hass Password;
+		String c="$2a$10$4rKRs4BEoCHTD.V1OqG5G.K0Ih5rjGZF8Lap1WSXRYiUIvkFSP4Qq";
+		Hasher.hashPass("abcd");
+		System.out.println(	Hasher.hashPass("abcd"));
+		assertEquals(c, Hasher.hashPass("abcd"));
+	}
+    @Test
+	public void test_classcheckPass1() {
+		//to check boolean class if inputted Hash Password is equal to the Hash Password of abcd;
+		String c="$2a$10$4rKRs4BEoCHTD.V1OqG5G.K0Ih5rjGZF8Lap1WSXRYiUIvkFSP4Qq";
+		//	System.out.println(	Hasher.hashPass("abcd"));
+		assertEquals(true,Hasher.checkPass("abcd",c));
+	}
+
+}
